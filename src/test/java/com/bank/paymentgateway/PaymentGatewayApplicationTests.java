32be261ada907bbb333a6dc4174d1e7873bc7a11
Khoa Phan <phanquockhoa99@gmail.com>
2025-07-21 23:23:07 +0700

    Initial commit with existing project files

---
+++ src/test/java/com/bank/paymentgateway/PaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.paymentgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
